11eba1edd3f9bee63ee5046b8d70c047f3ac5ec1
Thomas Genin <xt6.thomas.genin@gmail.com>
2014-02-01 17:03:37 -0800

    More functions

--- config/0.9.12/functionList.js
+++ config/0.9.12/functionList.js
@@ -3,22 +3,97 @@ var GraphiteConfig = GraphiteConfig || {};
 GraphiteConfig['0.9.12'] = GraphiteConfig['0.9.12'] || {};
 
 GraphiteConfig['0.9.12'].SerieFunctions = {
+    absolute:{},
+    alias:{
+        arg:[{
+            name: "newName",
+            type: "string"
+        }]
+    },
+    aliasByMetric:{},
+    aliasByNode:{
+        arg:[{
+            name: "node",
+            type: "number"
+        }]
+    },
+    aliasSub:{
+        arg:[{
+            name: "search",
+            type: "string"
+        },{
+            name: "replace",
+            type: "string"
+        }]
+    },
+    alpha:{
+        arg:[{
+            name: "alpha",
+            type: "float"
+        }]
+    },
+    areaBetween:{},
+    asPercent:{
+        arg:[{
+            name: "total",
+            type: "string",
+            required: false
+        }]
+    },
+    averageAbove:{
+        arg:[{
+            name: "n",
+            type: "integer"
+        }]
+    },
+    averageBelow:{
+        arg:[{
+            name: "n",
+            type: "integer"
+        }]
+    },
+    averageSeries: {},
+    avg: {
+        alias: 'averageSeries'
+    },
+    averageSeriesWithWildcards:{
+        arg:[{
+            name: "position",
+            type: "integer"
+        }]
+    },
+    cactiStyle:{
+        arg:[{
+            name: "system",
+            type: "string",
+            required: false
+        }]
+    },
+    color:{
+        arg:[{
+            name: "theColor",
+            type: "string"
+        }]
+    },
     groupByNode: {
         arg:[{
             name: "nodeNum",
-            type: "number",
-            required: true
+            type: "number"
         },{
             name: "callback",
-            type: "string",
-            required: true
+            type: "string"
+        }]
+    },
+    movingAverage: {
+        arg: [{
+            name: "windowSize",
+            type: "string"
         }]
     },
     summarize:{
         arg:[{
             name: "intervalString",
-            type: "string",
-            required: true
+            type: "string"
         },{
             name: "func",
             type: "select",
@@ -32,8 +107,5 @@ GraphiteConfig['0.9.12'].SerieFunctions = {
             required: false
         }]
     },
-    sortByMaxima:{
-
-    },
-    thomas:{}
+    sortByMaxima:{}
 };
